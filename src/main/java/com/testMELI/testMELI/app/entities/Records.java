6bc6fb0dfa85be0cb358d7aad9e7169e1929ec38
natisbar <48846184+natisbar@users.noreply.github.com>
2021-12-23 23:52:28 -0500

    ajuste en registros duplicados

--- src/main/java/com/testMELI/testMELI/app/entities/Records.java
+++ src/main/java/com/testMELI/testMELI/app/entities/Records.java
@@ -12,6 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,12 +25,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor  //Constructor con todos los argumentos
 @NoArgsConstructor   //Constructor vacio
 @Entity
-@Table(name="registros")
+@Table(name="registros", uniqueConstraints={@UniqueConstraint(columnNames={"dna"})})
 public class Records implements Serializable{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)  //genera automáticamente el ID
     private Integer id;
-    @Column(nullable=false,unique=true)
+    @Column(nullable=false)
     private String[] dna;
     private String specimen;
 }
